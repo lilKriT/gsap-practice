26a8d9a4c51c892d406cf7b0affe369f8b21c68d
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-23 01:36:14 +0200

    Added a grid animation.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-gsap.to(".box", {
-  duration: 3,
-  rotation: 360,
-  right: 200,
-  backgroundColor: "#b2fefa",
+gsap.to(".sphere", {
+  duration: 1,
+  scale: 0.2,
+  y: -100,
+  opacity: 0,
+  stagger: 0.2,
   repeat: -1,
   yoyo: true,
-  ease: "back",
+});
+
+gsap.to(".matrix .square", 1, {
+  scale: 0.1,
+  y: 60,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+  delay: 1,
+  stagger: {
+    amount: 1.5,
+    grid: "auto",
+    from: "center",
+  },
 });
